e3e40f313bd36ca15cd62fb94942e2286c30cf09
Rafaela Bertuol <rafabertuol02@gmail.com>
2024-08-21 19:57:57 +0000

    fact tabela ajusted

--- models/marts/fact_sales.sql
+++ models/marts/fact_sales.sql
@@ -1,28 +1,28 @@
 with
     customers as (
         select
-            sk_customer
+            sk_customer as fk_customer
             , customerid
         from {{ref('dim_customers')}} 
     )
 
     , creditcards as (
         select
-            sk_creditcard
+            sk_creditcard as fk_creditcard
             , creditcardid
         from {{ref('dim_creditcards')}}
     )
 
     , locations as (
         select
-            sk_shiptoaddress
+            sk_shiptoaddress as fk_shiptoaddress
             , shiptoaddressid
         from {{ref('dim_locations')}}
     )
 
     , products as (
         select
-            sk_product
+            sk_product as fk_product
             , productid
         from {{ref('dim_products')}}
     )
@@ -35,40 +35,33 @@ with
 
     , reasons as (
         select
-            sk_salesreason
+            sk_salesreason as fk_salesreason
             , salesorderid
-            , reason
+            , row_number() over (
+                partition by
+                    salesorderid
+                    , salesreasonid
+                order by salesreasonid desc
+            ) as dedup_tabela
         from {{ref('dim_salesreasons')}}
     )
 
+    /* for each orderid we can have more than one reason for the sale. The dedup was included to create the surrogate key for the fact table from the salesorderid */ 
+    
+    , dedup as (  
+        select *
+        from reasons
+        where dedup_tabela = 1
+    )
+
     , salesorderdetail as (
-        select
-            stg_salesorderdetail.salesorderid
-            , products.sk_product as fk_product
-            , stg_salesorderdetail.productid
-            , stg_salesorderdetail.orderqty
-            , stg_salesorderdetail.revenue
-            , stg_salesorderdetail.unitprice
-            , reasons.reason
-            , reasons.sk_salesreason as fk_salesreason
-        from {{ref('stg_sap__salesorderdetail')}} stg_salesorderdetail
-        left join products on stg_salesorderdetail.productid = products.productid
-        left join reasons on stg_salesorderdetail.salesorderid = reasons.salesorderid
+        select *
+        from {{ref('stg_sap__salesorderdetail')}} 
     )
 
     , salesorderheader as (
-        select
-            salesorderid
-            , customers.sk_customer as fk_customer
-            , creditcards.sk_creditcard as fk_creditcard
-            , locations.sk_shiptoaddress as fk_shiptoaddress
-            , stg_salesorderheader.salespersonid as fk_salesperson
-            , order_status
-            , order_date
-        from {{ref('stg_sap__salesorderheader')}} stg_salesorderheader
-        left join customers on stg_salesorderheader.customerid = customers.customerid
-        left join creditcards on stg_salesorderheader.creditcardid = creditcards.creditcardid
-        left join locations on stg_salesorderheader.shiptoaddressid = locations.shiptoaddressid
+        select *
+        from {{ref('stg_sap__salesorderheader')}} 
     )
 
     , final as (
@@ -76,36 +69,27 @@ with
             {{ surrogate_key(
                 'salesorderdetail.salesorderid'
             ) }} as sk_factsales -- Surrogate Key
-            , salesorderdetail.fk_product
-            , salesorderheader.fk_customer
-            , salesorderheader.fk_shiptoaddress
-            , salesorderheader.fk_creditcard
-            , salesorderheader.fk_salesperson
-            , salesorderdetail.fk_salesreason 
+            , products.fk_product
+            , customers.fk_customer
+            , locations.fk_shiptoaddress
+            , creditcards.fk_creditcard
+            , salesorderheader.salespersonid
+            , dedup.fk_salesreason 
             , salesorderdetail.salesorderid
             , salesorderdetail.unitprice
             , salesorderdetail.orderqty
             , salesorderdetail.revenue -- Total value of the sale, including product discount, without taxes and freight
-            , salesorderdetail.reason
             , salesorderheader.order_status
             , dates.date as order_date
         from salesorderdetail
         left join salesorderheader on salesorderdetail.salesorderid = salesorderheader.salesorderid
         left join dates on salesorderheader.order_date = dates.date 
+        left join products on salesorderdetail.productid = products.productid
+        left join dedup on salesorderdetail.salesorderid = dedup.salesorderid
+        left join customers on salesorderheader.customerid = customers.customerid
+        left join creditcards on salesorderheader.creditcardid = creditcards.creditcardid
+        left join locations on salesorderheader.shiptoaddressid = locations.shiptoaddressid
     )
 
-, dedup as (
-        select
-            *
-            , row_number() over (
-                partition by
-                    sk_factsales
-                    , order_date
-                order by order_date desc
-            ) as dedup_tabela
-        from final
-)
-
 select *
-from dedup
-where dedup_tabela = 1
+from final
