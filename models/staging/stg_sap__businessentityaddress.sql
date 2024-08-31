ced4fc1edd10a48bd62c5dc1fabf3b2d69c631b6
Rafaela Bertuol <rafabertuol02@gmail.com>
2024-08-31 17:07:49 +0000

    Deleted stg tables with no use

--- models/staging/stg_sap__businessentityaddress.sql
+++
@@ -1,9 +0,0 @@
-with
-    source as (
-        select
-            cast(addressid as int) as addressid           
-            , cast(businessentityid as int) as businessentityid
-        from {{ source('sap_adw', 'businessentityaddress') }}
-    )
-select *
-from source
