0975ee7d788ba04da9dd3d239c66b75dca5caf99
Mafra Michele <michele.mafra2@gmail.com>
2024-12-27 22:40:45 +0000

    commit stg

---
+++ models/staging/sap_adw/stg_sap_adw__addresstype.sql
@@ -0,0 +1,15 @@
+with 
+
+    tipo_de_endereco as (
+
+        select 
+            cast(addresstypeid as int) as id_tipo_endereco
+            , name as nome
+            , rowguid as linha_guia
+            , cast(format_timestamp('%Y-%m-%d %H:%M:%S', cast(modifieddate as timestamp)) as timestamp) as data_modificacao
+        from {{ source('sap_adw', 'addresstype') }}
+
+    )
+
+select * 
+from tipo_de_endereco 
---
+++ models/staging/sap_adw/stg_sap_adw__contato_da_entidade_empresarial.sql
@@ -0,0 +1,18 @@
+with 
+
+    contato_da_entidade_empresarial as (
+
+        select 
+            cast(businessentityid as int) as id_entidadede_empresarial
+            , cast(personid as int) as id_pessoa
+            , cast(contacttypeid as int) id_tipo_contato
+            , rowguid as linha_guia
+            , cast(format_timestamp('%Y-%m-%d %H:%M:%S', cast(modifieddate as timestamp)) as timestamp) as dt_modificacao
+        from {{ source('sap_adw', 'businessentitycontact') }}
+
+    )
+
+
+
+select *
+from contato_da_entidade_empresarial
